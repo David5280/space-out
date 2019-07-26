8b4f36002826d3d1cdab00f657c5776bacda2c21
David Engel <Davidjengel2012@gmail.com>
2019-07-26 17:40:22 -0600

    Continue work on planet info pages

--- src/PlanetInfo/PlanetInfo.js
+++ src/PlanetInfo/PlanetInfo.js
@@ -5,8 +5,14 @@ const PlanetInfo = ({ planet }) => {
   return (
     <section className='planet-info'>
       <img src={planet.image} alt={`${planet.title}`} className='planet-info-image'/>
-      <h2 className='planet-info-title planet-info-text'>{planet.title}</h2>
-      <p className='planet-info-description planet-info-text'>{planet.description}</p>
+      <article className='planet-info-left-top'>
+        <h2 className='planet-info-title planet-info-text'>{planet.title}</h2>
+        <p className='planet-info-description planet-info-text'>{planet.description}</p>
+      </article>
+      <article className='planet-info-left-bottom'>
+        <h3>Atmosphere</h3>
+        <p>{planet.atmosphere}</p>
+      </article>
     </section>
   )
 }

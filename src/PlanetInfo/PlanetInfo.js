d7188bc7849a664cf71a08422098a340726b4eb7
David Engel <Davidjengel2012@gmail.com>
2019-07-26 18:07:10 -0600

    Planet details left side near completed

--- src/PlanetInfo/PlanetInfo.js
+++ src/PlanetInfo/PlanetInfo.js
@@ -10,8 +10,18 @@ const PlanetInfo = ({ planet }) => {
         <p className='planet-info-description planet-info-text'>{planet.description}</p>
       </article>
       <article className='planet-info-left-bottom'>
-        <h3>Atmosphere</h3>
+        <h3>Atmosphere:</h3>
         <p>{planet.atmosphere}</p>
+        <h3>Miles From the Sun:</h3>
+        <p>{planet.milesFromSun}</p>
+        <h3>Travel Time:</h3>
+        <p>{planet.travelTime}</p>
+        <h3>Gravity:</h3>
+        <p>{planet.gravity} times Earth's Gravity</p>
+        <h3>Number of Moons:</h3>
+        <p>{planet.moons}</p>
+        <h3>1 Year:</h3>
+        <p>{planet.sunRevolution} Earth days.</p>
       </article>
     </section>
   )

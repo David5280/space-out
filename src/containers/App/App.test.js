ca8c78ddb5988cbe653a1d131ca9fba263cba3f6
David Engel <Davidjengel2012@gmail.com>
2019-07-27 11:18:31 -0600

    Install testing dependencies, setup app for testing

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  describe('App Component', () => {
+    let wrapper;
+    beforeEach(() => {
+      const props = {
+        loadPlanets: jest.fn(),
+        planets: [
+          {
+            title: 'earth',
+            status: 'baking'
+          },
+          {
+            title: 'mars',
+            status: 'preparing for colinization'
+          }
+        ]
+      }
+      wrapper = shallow(<App {...props} />, {disableLifecycleMethods: true})
+    })
+  })
+})

5ff90491c99d3c8d6e842044f1d3bdaebb53bb63
David Engel <Davidjengel2012@gmail.com>
2019-07-28 15:08:59 -0600

    Add form, add handleChange function

--- src/containers/StatsForm/StatsForm.js
+++ src/containers/StatsForm/StatsForm.js
@@ -9,24 +9,35 @@ export class StatsForm extends React.Component {
       weight: ''
     }
   }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
   render() {
     return(
       <section className='form-container'>
         <form className='stats-form'>
         <input 
-          type='text' 
           className='stats-form-input' 
-          placeholder='First Name...' 
+          name='name' 
+          placeholder='First Name...'
+          type='text'
+          onChange={this.handleChange} 
         />
         <input 
-          type='text' 
           className='stats-form-input' 
+          name='age' 
           placeholder='Age...' 
+          type='text'
+          onChange={this.handleChange} 
         />
         <input 
-          type='text' 
           className='stats-form-input' 
+          name='weight'
           placeholder='Weight...  (lbs)' 
+          type='text'
+          onChange={this.handleChange} 
         />
         <button className='stats-form-input'>Submit!</button>
         </form>

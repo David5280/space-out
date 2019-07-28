5ca88dee5593d5ff05d6b0814f2a22aedad5dff2
David Engel <Davidjengel2012@gmail.com>
2019-07-28 13:29:37 -0600

    Add dropdown menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ planets }) => {
+  const menuItems = planets.map(planet => {
+    return (
+      <NavLink to={`/${planet.title}`} key={planet.id}>{planet.title}</NavLink>
+    )
+  })
   return (
     <header>
       <NavLink to='/'>
@@ -9,10 +14,16 @@ const Header = () => {
           SPACE-OUT
         </h1>
       </NavLink>
-      <img 
-        className='header-menu-icon' 
-        src='https://i1.wp.com/royalposthumus.com/images/white_menu_icon.png' alt='menu-icon' 
-      />
+      <div className='header-dropdown'>
+        <img 
+          className='header-menu-icon' 
+          src='https://i1.wp.com/royalposthumus.com/images/white_menu_icon.png' alt='menu-icon' 
+        />
+        <div className='dropdown-content'>
+          <NavLink to='/'>Home</NavLink>
+          {menuItems}
+        </div>
+      </div>
     </header>
   )
 }
